f889387b3a005d1a526fbee7464a6faf06ceb83b
jarivaht <jarivahtola@gmail.com>
2026-05-08 18:36:39 +0300

    fix url

--- src/main.js
+++ src/main.js
@@ -45,7 +45,7 @@ app.innerHTML = /*html*/ `
           <span class="heading-sm md:heading-md lg:heading-lg 3xl:heading-xl select-none" aria-hidden="true">&nbsp;</span>
           ${Heading({ text: "Opening<br>Soon<br>13.6.2026", tag: "h2", className: "mb-8 md:mb-12" })}
           <div class="">
-            ${Button({ href: "varaukset.tokoihakaniemi.fi", label: "Wine tasting reservations →" })}
+            ${Button({ href: "https://varaukset.tokoihakaniemi.fi", label: "Wine tasting reservations →" })}
           </div>
         </div>
       </div>
